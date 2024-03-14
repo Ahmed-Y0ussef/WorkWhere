39affa8586628aec02b4670ec59b50c1c250ae65
Ahmed-Y0ussef <155473815+Ahmed-Y0ussef@users.noreply.github.com>
2024-03-15 01:59:15 +0200

    test

---
+++ WorkWhere/Core/Interfaces/IGenericRepo.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepo<T> where T : BaseEntity
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+       
+    }
+}
---
+++ WorkWhere/Infrastructure/Repositories/GenericRepo.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Dbcontext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    internal class GenericRepo<T> : IGenericRepo<T> where T : BaseEntity
+    {
+        ApplicationDbContext AppContext;
+        public GenericRepo(ApplicationDbContext _AppContext)
+        {
+            AppContext = _AppContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await AppContext.Set<T>().ToListAsync();
+       
+       
+    }
+}
