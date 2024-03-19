c4f3155901f74a61136cc373be91733f1b333843
Ahmed-Y0ussef <155473815+Ahmed-Y0ussef@users.noreply.github.com>
2024-03-19 06:43:36 +0200

    t7

--- WorkWhere/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ WorkWhere/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f31e3c19e153e54217a9beb69b90d2dc17877f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886e71580038d1051b8204b8b369afced8e34efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
